6d32bf4b01c3fbe9b03e5d0dc1942ec4b841c876
thexaib <thexeb@gmail.com>
2019-08-08 16:23:10 +0500

    1.0

---
+++ src/GenericArray.php
@@ -0,0 +1,127 @@
+<?php
+
+namespace thexaib\model;
+
+
+class GenericArray implements \ArrayAccess, \Iterator
+{
+
+    public function __construct($row = null)
+    {
+        if ($row != null) {
+            $this->loadFromArray($row);
+        }
+
+    }
+
+    public function loadFromArray($row)
+    {
+        foreach ($row as $k => $v) {
+            if ($v != null && $v == '') $v = null;
+            $this->$k = $v;
+        }
+    }
+
+
+    public function __set($var, $val)
+    {
+        if ($val != null && $val == '') $val = null;
+        $this->container[$var] = $val;
+    }
+
+    public function __get($var)
+    {
+        if (isset($this->container[$var])) {
+            return $this->container[$var];
+        } else {
+            return null;
+        }
+    }
+
+    public function __unset($var)
+    {
+        unset($this->container[$var]);
+    }
+
+    public function toArray()
+    {
+        return $this->container;
+    }
+
+    public function toJSON()
+    {
+        //todo: Check JSON encode option
+//        return json_encode($this->toArray(), JSON_NUMERIC_CHECK);
+        return json_encode($this->toArray(), JSON_PRETTY_PRINT);
+    }
+
+    public function __toString()
+    {
+        return $this->toJSON();
+    }
+
+    // =====================ArrayAccess Interface
+    protected $container = array();
+
+    public function offsetSet($offset, $value)
+    {
+        if ($value != null && $value == '') $value = null;
+
+        if (is_null($offset)) {
+            $this->container[] = $value;
+        } else {
+            $this->container[$offset] = $value;
+        }
+    }
+
+    public function offsetExists($offset)
+    {
+        return isset($this->container[$offset]);
+    }
+
+    public function offsetUnset($offset)
+    {
+        unset($this->container[$offset]);
+    }
+
+    /**
+     * @param mixed $offset
+     * @return GenericArray
+     */
+    public function offsetGet($offset)
+    {
+        return isset($this->container[$offset]) ? $this->container[$offset] : null;
+    }
+    //end::=====================ArrayAccess Interface
+    //Start::=====================Iterator Interface
+    public function current()
+    {
+        return current($this->container);
+    }
+
+    public function next()
+    {
+        return next($this->container);
+    }
+
+    public function key()
+    {
+        return key($this->container);
+    }
+
+    public function valid()
+    {
+        $key = key($this->container);
+        $var = ($key !== NULL && $key !== FALSE);
+        return $var;
+    }
+
+    public function rewind()
+    {
+        reset($this->container);
+    }
+    //end::=====================Iterator Interface
+}
+
+
+
---
+++ src/GenericArrayMulti.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace thexaib\model;
+
+
+class GenericArrayMulti extends GenericArray
+{
+    public function __set($var, $val)
+    {
+        if ($val != null && $val == '') $val = null;
+        $this->offsetSet($var,$val);
+//        $this->container[$var] = $this->convertValue($val);
+    }
+
+    public function toArray()
+    {
+        $out=[];
+        foreach ($this->container as $key=>$value) {
+            if($value instanceof GenericArrayMulti)
+            {
+                $out[$key]=$value->toArray();
+            }
+            else if($value!=null){ //todo: if no check,all the table entries with null values be returned
+                $out[$key] = $value;
+            }
+        }
+        return $out;
+    }
+
+
+
+
+    public function offsetSet($offset, $value)
+    {
+        if ($value != null && $value == '') $value = null;
+
+        if (is_null($offset)) {
+            $this->container[] = $this->convertValue($value);
+        } else {
+            $this->container[$offset] = $this->convertValue($value);
+        }
+    }
+    protected function convertValue($val)
+    {
+
+        if(is_array($val))
+        {
+            $sub=new GenericArrayMulti($val);
+        }
+        elseif (is_object($val))
+        {
+            $sub=new GenericArrayMulti();
+            foreach ($val as $k => $v) {
+                if ($v != null && $v == '') $v = null;
+                $sub->{$k} = $v;
+            }
+        }else
+        {
+            $sub=$val;
+        }
+
+        return $sub;
+    }
+
+}
